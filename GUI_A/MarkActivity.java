ffef076a5922637c2ab99c53523c0d16a41edc3f
DingoMC <95879619+DingoMC@users.noreply.github.com>
2023-03-21 15:25:24 +0100

    Create MarkActivity.java

---
+++ GUI_A/MarkActivity.java
@@ -0,0 +1,14 @@
+package pl.pollub.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MarkActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mark);
+    }
+}
