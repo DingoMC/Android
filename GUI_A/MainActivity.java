e85e963e67767eb9cf148df3675a396a64aa525d
DingoMC <95879619+DingoMC@users.noreply.github.com>
2023-03-21 15:23:51 +0100

    Update MainActivity.java

--- GUI_A/MainActivity.java
+++ GUI_A/MainActivity.java
@@ -1,5 +1,6 @@
 package pl.pollub.lab1;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -30,13 +31,20 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.toolbar);
 
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
+
+        binding.getRoot().getChildAt(1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, MarkActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
